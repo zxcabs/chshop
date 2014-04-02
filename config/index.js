bbccb85d1c2157e89afb9b67e2f1a7a3b581606d
Evgeny Reznichenko <kusakyky@gmail.com>
2014-04-03 01:26:06 +0400

    fix Исправлена опечатка в config

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@
 
 var
 	path = require('path'),
-	configDirName = process.env.NODE_ENV ? process.NODE_ENV.toLowerCase(): 'development';
+	configDirName = (process.env.NODE_ENV || 'development').toLowerCase();
 
 console.log('load "%s" config', configDirName);
 module.exports = require(path.join(__dirname, configDirName));
